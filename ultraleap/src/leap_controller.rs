af4de3fc601a079db4ce87dac828d549ab11cedf
Daniel Toplak <homer.simpson123@gmx.de>
2023-08-10 21:22:11 +0200

    feat: add Palm to Hand

--- ultraleap/src/leap_controller.rs
+++ ultraleap/src/leap_controller.rs
@@ -17,39 +17,48 @@ pub struct LeapController {
     tracking_event_receiver: Option<Receiver<TrackingEvent>>,
 }
 
+impl Default for LeapController {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
 impl LeapController {
     pub fn new() -> LeapController {
-        LeapController {
+        let mut leap_controller = LeapController {
             running: false,
             polling_thread: None,
             stop_sender: None,
             tracking_event_receiver: None,
-        }
+        };
+        leap_controller.open_connection();
+        leap_controller
     }
 
-    pub fn open_connection(&mut self) {
+    fn open_connection(&mut self) {
         if self.running {
             println!("already running");
             return;
         }
+        self.running = true;
 
         let (stop_sender, stop_receiver) = mpsc::channel();
         let (tracking_event_sender, tracking_event_receiver) = mpsc::channel();
 
         self.stop_sender = Some(stop_sender);
         self.tracking_event_receiver = Some(tracking_event_receiver);
-
         self.polling_thread = Some(thread::spawn(move || {
             println!("start polling thread");
             unsafe {
                 const CONFIG: MaybeUninit<LEAP_CONNECTION_CONFIG> = MaybeUninit::uninit();
                 let mut connection: LEAP_CONNECTION = ptr::null_mut();
                 let mut running = false;
+                println!("creating and opening connection");
                 if LeapCreateConnection(CONFIG.as_ptr(), &mut connection)
                     == _eLeapRS_eLeapRS_Success
                     && LeapOpenConnection(connection) == _eLeapRS_eLeapRS_Success
                 {
-                    println!("success");
+                    println!("connection created and open");
                     running = true;
                 }
 
@@ -90,7 +99,15 @@ impl LeapController {
                             if raw_tracking_event.nHands > 0 {
                                 for i in 0..raw_tracking_event.nHands {
                                     let raw_hand = *raw_tracking_event.pHands.offset(i as isize);
-                                    let hand = Hand { id: raw_hand.id };
+                                    let raw_palm = raw_hand.palm;
+                                    let palm = Palm {
+                                        position: raw_palm.position.__bindgen_anon_1.v,
+                                        orientation: raw_palm.orientation.__bindgen_anon_1.v,
+                                    };
+                                    let hand = Hand {
+                                        id: raw_hand.id,
+                                        palm,
+                                    };
                                     tracking_event.hands.push(hand);
                                 }
                             }
@@ -120,7 +137,7 @@ impl LeapController {
         }));
     }
 
-    pub fn close_connection(&mut self) {
+    fn close_connection(&mut self) {
         self.stop_sender.take().unwrap().send(true).unwrap();
         self.polling_thread.take().unwrap().join().unwrap();
     }
@@ -132,6 +149,15 @@ impl LeapController {
                 _ => None,
             },
             _ => None,
+        }
+    }
+}
+
+impl Drop for LeapController {
+    fn drop(&mut self) {
+        if self.running {
+            println!("closing connection");
+            self.close_connection();
         }
     }
 }
--- ultraleap/src/tracking_event.rs
+++ ultraleap/src/tracking_event.rs
@@ -1,5 +1,11 @@
+pub struct Palm {
+    pub position: [f32; 3],
+    pub orientation: [f32; 4],
+}
+
 pub struct Hand {
     pub id: u32,
+    pub palm: Palm,
 }
 
 pub struct TrackingEvent {
